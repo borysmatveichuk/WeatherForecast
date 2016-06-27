7335ee34aac6eaa7e578832a7dd6cf10445cf8f4
unknown <boris.matveychuck@gmail.com>
2016-06-28 00:06:04 +0300

    - rename fields in schema

--- app/src/main/java/net/borkiss/weatherforecast/db/WeatherBaseHelper.java
+++ app/src/main/java/net/borkiss/weatherforecast/db/WeatherBaseHelper.java
@@ -23,7 +23,7 @@ public class WeatherBaseHelper extends SQLiteOpenHelper {
         sqLiteDatabase.execSQL("create table " + PlacesTable.NAME + "(" +
                 " _id integer primary key autoincrement, " +
                 PlacesTable.Cols.NAME + ", " +
-                PlacesTable.Cols.ID + ", " +
+                PlacesTable.Cols.CITY_ID + ", " +
                 PlacesTable.Cols.COUNTRY + ", " +
                 PlacesTable.Cols.COORD_LON + ", " +
                 PlacesTable.Cols.COORD_LAT +
@@ -32,7 +32,7 @@ public class WeatherBaseHelper extends SQLiteOpenHelper {
 
         sqLiteDatabase.execSQL("create table " + CurrentWeatherTable.NAME + "(" +
                 " _id integer primary key autoincrement, " +
-                CurrentWeatherTable.Cols.PLACE + ", " +
+                CurrentWeatherTable.Cols.PLACE_ID + ", " +
                 CurrentWeatherTable.Cols.TIME + ", " +
                 CurrentWeatherTable.Cols.DOCUMENT +
                 ")"
@@ -40,7 +40,7 @@ public class WeatherBaseHelper extends SQLiteOpenHelper {
 
         sqLiteDatabase.execSQL("create table " + ForecastFiveDayTable.NAME + "(" +
                 " _id integer primary key autoincrement, " +
-                ForecastFiveDayTable.Cols.PLACE + ", " +
+                ForecastFiveDayTable.Cols.PLACE_ID + ", " +
                 ForecastFiveDayTable.Cols.TIME + ", " +
                 ForecastFiveDayTable.Cols.DOCUMENT +
                 ")"
--- app/src/main/java/net/borkiss/weatherforecast/db/WeatherDbSchema.java
+++ app/src/main/java/net/borkiss/weatherforecast/db/WeatherDbSchema.java
@@ -5,7 +5,7 @@ public class WeatherDbSchema {
         public static final String NAME = "currentWeather";
 
         public static final class Cols {
-            public static final String PLACE = "place";
+            public static final String PLACE_ID = "place";
             public static final String TIME = "time";
             public static final String DOCUMENT = "document";
         }
@@ -15,7 +15,7 @@ public class WeatherDbSchema {
         public static final String NAME = "forecastFiveDay";
 
         public static final class Cols {
-            public static final String PLACE = "place";
+            public static final String PLACE_ID = "place";
             public static final String TIME = "time";
             public static final String DOCUMENT = "document";
         }
@@ -26,7 +26,7 @@ public class WeatherDbSchema {
 
         public static final class Cols {
             public static final String NAME = "name";
-            public static final String ID = "id";
+            public static final String CITY_ID = "id";
             public static final String COUNTRY = "country";
             public static final String COORD_LON = "coordLon";
             public static final String COORD_LAT = "coordLat";
