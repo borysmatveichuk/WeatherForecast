03db7d6f576f98944bd6b7d14ed841ad95397ff9
unknown <boris.matveychuck@gmail.com>
2016-06-28 00:07:47 +0300

    - add tests

--- app/src/androidTest/java/net/borkiss/weatherforecast/DatabaseTest.java
+++ app/src/androidTest/java/net/borkiss/weatherforecast/DatabaseTest.java
@@ -8,6 +8,8 @@ import android.support.test.runner.AndroidJUnit4;
 import net.borkiss.weatherforecast.api.WeatherStation;
 import net.borkiss.weatherforecast.db.WeatherBaseHelper;
 import net.borkiss.weatherforecast.db.WeatherDbSchema;
+import net.borkiss.weatherforecast.model.CurrentWeatherDTO;
+import net.borkiss.weatherforecast.model.ForecastFiveDayDTO;
 import net.borkiss.weatherforecast.model.Place;
 
 import org.junit.After;
@@ -40,9 +42,15 @@ public class DatabaseTest {
     }
 
     @Test
-    public void insertSelectPlaces() throws Exception {
+    public void databaseInsertSelect() throws Exception {
+        insertSelectPlaces();
+        insertSelectCurrentWeatherDTO();
+        insertSelectForecastFiveDayDTO();
+    }
 
-        Place place = new Place();ContentValues values = new ContentValues();
+    private void insertSelectPlaces() throws Exception {
+
+        Place place = new Place();
         place.setName("Nikolaev");
         place.setCountry("UA");
         place.setId(123456);
@@ -60,6 +68,57 @@ public class DatabaseTest {
             assertThat(p.getLatitude(), is(123.456f));
             assertThat(p.getLongitude(), is(789.012f));
         }
+
+    }
+
+
+    private void insertSelectCurrentWeatherDTO() throws Exception {
+        CurrentWeatherDTO dto = new CurrentWeatherDTO();
+        dto.setDocument("dto");
+        dto.setTime(123456789);
+        dto.setPlaceId(111);
+
+        CurrentWeatherDTO dto2 = new CurrentWeatherDTO();
+        dto2.setDocument("dto2");
+        dto2.setTime(99999999L);
+        dto2.setPlaceId(222);
+
+
+        sqLiteDatabase.addCurrentWeatherDTO(dto);
+        sqLiteDatabase.addCurrentWeatherDTO(dto2);
+
+        List<CurrentWeatherDTO> list = sqLiteDatabase.getListCurrentWeatherDTO();
+
+        assertThat(list.size(), is(2));
+
+        assertTrue(list.get(1).getDocument().equals("dto2"));
+        assertThat(list.get(1).getTime(), is(99999999L));
+        assertThat(list.get(1).getPlaceId(), is(222));
+
+    }
+
+    private void insertSelectForecastFiveDayDTO() throws Exception {
+        ForecastFiveDayDTO dto = new ForecastFiveDayDTO();
+        dto.setDocument("dto");
+        dto.setTime(1111L);
+        dto.setPlaceId(111);
+
+        ForecastFiveDayDTO dto2 = new ForecastFiveDayDTO();
+        dto2.setDocument("dto2");
+        dto2.setTime(2222L);
+        dto2.setPlaceId(222);
+
+
+        sqLiteDatabase.addForecastFiveDayDTO(dto);
+        sqLiteDatabase.addForecastFiveDayDTO(dto2);
+
+        List<ForecastFiveDayDTO> list = sqLiteDatabase.getListForecastFiveDayDTO();
+
+        assertThat(list.size(), is(2));
+
+        assertTrue(list.get(1).getDocument().equals("dto2"));
+        assertThat(list.get(1).getTime(), is(2222L));
+        assertThat(list.get(1).getPlaceId(), is(222));
 
     }
 }
