20891d1c593be4d8ffdeee9bfc7b7f53651a8e04
unknown <boris.matveychuck@gmail.com>
2016-06-29 00:42:54 +0300

    - add search to toolbar

--- app/src/main/java/net/borkiss/weatherforecast/fragment/PlaceListFragment.java
+++ app/src/main/java/net/borkiss/weatherforecast/fragment/PlaceListFragment.java
@@ -1,15 +1,29 @@
 package net.borkiss.weatherforecast.fragment;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import net.borkiss.weatherforecast.R;
 
 public class PlaceListFragment extends Fragment {
+
+    private static final String TAG = PlaceListFragment.class.getSimpleName();
+
+    private TextView textView;
 
     public static PlaceListFragment newInstance() {
 
@@ -20,9 +34,47 @@ public class PlaceListFragment extends Fragment {
         return fragment;
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_place_list, container, false);
     }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        textView = (TextView) view.findViewById(R.id.txtPlace);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_place_list, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.menu_item_search);
+        final SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                Log.d(TAG, "QueryTextSubmit: " + s);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                Log.d(TAG, "QueryTextChange: " + s);
+                textView.setText(s);
+                return false;
+            }
+        });
+
+    }
+
 }
