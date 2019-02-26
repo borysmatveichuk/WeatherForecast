b0484908467c68934da6013ce13a85da32035650
borysmatveichuk <boris.matveychuck@gmail.com>
2019-02-27 00:44:57 +0200

    - import static tables

--- app/src/main/java/net/borkiss/weatherforecast/db/CurrentWeatherCursorWrapper.java
+++ app/src/main/java/net/borkiss/weatherforecast/db/CurrentWeatherCursorWrapper.java
@@ -3,10 +3,11 @@ package net.borkiss.weatherforecast.db;
 import android.database.Cursor;
 import android.database.CursorWrapper;
 
-import net.borkiss.weatherforecast.db.WeatherDbSchema.CurrentWeatherTable;
 import net.borkiss.weatherforecast.model.CurrentWeather;
 
 import java.util.Date;
+
+import static net.borkiss.weatherforecast.db.WeatherDbSchema.CurrentWeatherTable.Cols.*;
 
 public class CurrentWeatherCursorWrapper extends CursorWrapper {
 
@@ -21,21 +22,21 @@ public class CurrentWeatherCursorWrapper extends CursorWrapper {
 
     public CurrentWeather getCurrentWeather() {
 
-        int placeId = getInt(getColumnIndex(CurrentWeatherTable.Cols.PLACE_ID));
-        long time = getLong(getColumnIndex(CurrentWeatherTable.Cols.TIME));
-        int conditionId = getInt(getColumnIndex(CurrentWeatherTable.Cols.CONDITION_ID));
-        String weatherMine = getString(getColumnIndex(CurrentWeatherTable.Cols.WEATHER_MAIN));
-        String weatherDescription = getString(getColumnIndex(CurrentWeatherTable.Cols.WEATHER_DESCRIPTION));
-        float temperature = getFloat(getColumnIndex(CurrentWeatherTable.Cols.TEMPERATURE));
-        float pressure = getFloat(getColumnIndex(CurrentWeatherTable.Cols.PRESSURE));
-        int humidity = getInt(getColumnIndex(CurrentWeatherTable.Cols.HUMIDITY));
-        float minTemperature = getFloat(getColumnIndex(CurrentWeatherTable.Cols.MIN_TEMPERATURE));
-        float maxTemperature = getFloat(getColumnIndex(CurrentWeatherTable.Cols.MAX_TEMPERATURE));
-        float windSpeed = getFloat(getColumnIndex(CurrentWeatherTable.Cols.WIND_SPEED));
-        float windDegree = getFloat(getColumnIndex(CurrentWeatherTable.Cols.WIND_DEGREE));
-        int clouds = getInt(getColumnIndex(CurrentWeatherTable.Cols.CLOUDS));
-        long sunrise = getLong(getColumnIndex(CurrentWeatherTable.Cols.SUNRISE));
-        long sunset = getLong(getColumnIndex(CurrentWeatherTable.Cols.SUNSET));
+        int placeId = getInt(getColumnIndex(PLACE_ID));
+        long time = getLong(getColumnIndex(TIME));
+        int conditionId = getInt(getColumnIndex(CONDITION_ID));
+        String weatherMine = getString(getColumnIndex(WEATHER_MAIN));
+        String weatherDescription = getString(getColumnIndex(WEATHER_DESCRIPTION));
+        float temperature = getFloat(getColumnIndex(TEMPERATURE));
+        float pressure = getFloat(getColumnIndex(PRESSURE));
+        int humidity = getInt(getColumnIndex(HUMIDITY));
+        float minTemperature = getFloat(getColumnIndex(MIN_TEMPERATURE));
+        float maxTemperature = getFloat(getColumnIndex(MAX_TEMPERATURE));
+        float windSpeed = getFloat(getColumnIndex(WIND_SPEED));
+        float windDegree = getFloat(getColumnIndex(WIND_DEGREE));
+        int clouds = getInt(getColumnIndex(CLOUDS));
+        long sunrise = getLong(getColumnIndex(SUNRISE));
+        long sunset = getLong(getColumnIndex(SUNSET));
 
         CurrentWeather weather = new CurrentWeather();
         weather.setPlaceId(placeId);
--- app/src/main/java/net/borkiss/weatherforecast/db/ForecastFiveDayCursorWrapper.java
+++ app/src/main/java/net/borkiss/weatherforecast/db/ForecastFiveDayCursorWrapper.java
@@ -3,10 +3,11 @@ package net.borkiss.weatherforecast.db;
 import android.database.Cursor;
 import android.database.CursorWrapper;
 
-import net.borkiss.weatherforecast.db.WeatherDbSchema.ForecastFiveDayTable;
 import net.borkiss.weatherforecast.model.ForecastFiveDay;
 
 import java.util.Date;
+
+import static net.borkiss.weatherforecast.db.WeatherDbSchema.ForecastFiveDayTable.Cols.*;
 
 public class ForecastFiveDayCursorWrapper extends CursorWrapper {
 
@@ -21,19 +22,19 @@ public class ForecastFiveDayCursorWrapper extends CursorWrapper {
 
     public ForecastFiveDay getForecastFiveDay() {
 
-        int placeId = getInt(getColumnIndex(ForecastFiveDayTable.Cols.PLACE_ID));
-        long time = getLong(getColumnIndex(ForecastFiveDayTable.Cols.TIME));
-        int conditionId = getInt(getColumnIndex(ForecastFiveDayTable.Cols.CONDITION_ID));
-        String weatherMine = getString(getColumnIndex(ForecastFiveDayTable.Cols.WEATHER_MAIN));
-        String weatherDescription = getString(getColumnIndex(ForecastFiveDayTable.Cols.WEATHER_DESCRIPTION));
-        float temperature = getFloat(getColumnIndex(ForecastFiveDayTable.Cols.TEMPERATURE));
-        float pressure = getFloat(getColumnIndex(ForecastFiveDayTable.Cols.PRESSURE));
-        int humidity = getInt(getColumnIndex(ForecastFiveDayTable.Cols.HUMIDITY));
-        float minTemperature = getFloat(getColumnIndex(ForecastFiveDayTable.Cols.MIN_TEMPERATURE));
-        float maxTemperature = getFloat(getColumnIndex(ForecastFiveDayTable.Cols.MAX_TEMPERATURE));
-        float windSpeed = getFloat(getColumnIndex(ForecastFiveDayTable.Cols.WIND_SPEED));
-        float windDegree = getFloat(getColumnIndex(ForecastFiveDayTable.Cols.WIND_DEGREE));
-        int clouds = getInt(getColumnIndex(ForecastFiveDayTable.Cols.CLOUDS));
+        int placeId = getInt(getColumnIndex(PLACE_ID));
+        long time = getLong(getColumnIndex(TIME));
+        int conditionId = getInt(getColumnIndex(CONDITION_ID));
+        String weatherMine = getString(getColumnIndex(WEATHER_MAIN));
+        String weatherDescription = getString(getColumnIndex(WEATHER_DESCRIPTION));
+        float temperature = getFloat(getColumnIndex(TEMPERATURE));
+        float pressure = getFloat(getColumnIndex(PRESSURE));
+        int humidity = getInt(getColumnIndex(HUMIDITY));
+        float minTemperature = getFloat(getColumnIndex(MIN_TEMPERATURE));
+        float maxTemperature = getFloat(getColumnIndex(MAX_TEMPERATURE));
+        float windSpeed = getFloat(getColumnIndex(WIND_SPEED));
+        float windDegree = getFloat(getColumnIndex(WIND_DEGREE));
+        int clouds = getInt(getColumnIndex(CLOUDS));
 
         ForecastFiveDay forecast = new ForecastFiveDay();
         forecast.setPlaceId(placeId);
